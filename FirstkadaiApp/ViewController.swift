9120c171e6ad3213d560317c25ef207efd7763ee
t.tetsuya <tetuyatt522@gmail.com>
2023-05-14 13:23:59 +0900

    Initial Commit

---
+++ FirstkadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstkadaiApp
+//
+//  Created by 富樫　哲也 on 2023/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
